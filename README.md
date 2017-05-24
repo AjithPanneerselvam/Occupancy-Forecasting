b2db4ed24b661df2594ebff6c20f924fb1c4dcd8
AjithPanneerselvam <ajithpanneerselvam20@gmail.com>
2017-05-23 23:30:51 -0600

    Cost Function

---
+++ README.md
@@ -0,0 +1,6 @@
+# Occupancy-Forecasting
+
+- Linear Regression model (Multi-variable) to forecast the number of occupants in a building at a given point of time.
+
+- **Input:** Day, Month, Weather, Special Impact Factor
+- **Output:** Occupants
---
+++ costFunction.m
@@ -0,0 +1,29 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+
+m = length(y); % number of training examples
+
+J_history = zeros(num_iters, 1);
+
+
+
+for iter = 1:num_iters
+
+
+	A = X * theta - y;  % (m x 1 vector)
+
+	delta = 1 / m * (A' * X)';  % ' ((n+1) x 1 vector)
+
+	theta = theta - (alpha * delta); % ' ((n+1) x 1 vector)
+  
+
+	cost = computeCostMulti(X, y, theta);
+
+	J_history(iter) = cost;
+
+
+
+end
+
+
+
+end
